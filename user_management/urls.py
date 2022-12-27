5d991e00da5a0be53cb44cb58bff1aad03c65059
Artur <archibay16@gmail.com>
2022-12-27 02:22:38 +0200

    in process

--- user_management/urls.py
+++ user_management/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
 
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("user-detail/<int:pk>/", views.UserDetailView.as_view(), name="user_detail"),
+    path("user-update/<int:pk>/", views.UserUpdateView.as_view(), name="user_update"),
 ]
--- user_management/views.py
+++ user_management/views.py
@@ -2,7 +2,9 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from django.contrib.auth.views import FormView
 from django.urls import reverse_lazy
-
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from user_management.forms import RegistrationForm
 
 
@@ -16,3 +18,20 @@ class RegistrationView(FormView):
         user = authenticate(self.request, username=user.username, password=form.cleaned_data.get('password1'))
         login(self.request, user)
         return super(RegistrationView, self).form_valid(form)
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'registration/user_detail.html'
+
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    template_name = 'registration/user_update.html'
+    success_url = reverse_lazy('blog:index')
+
+    def get_object(self, queryset=None):
+        user = self.request.user
+        return user
+
